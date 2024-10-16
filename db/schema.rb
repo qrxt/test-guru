b32805fd32eb658ec9390e560cfb7ad22ea53a02
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2024-10-16 05:51:38 +0300

    feat: добавление модели пользователя

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_16_023208) do
+ActiveRecord::Schema.define(version: 2024_10_16_024935) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
