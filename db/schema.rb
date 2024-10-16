f360a172a9f40cb35a9bbdcee324406f7d85f606
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2024-10-16 05:55:55 +0300

    feat: добавление модели теста

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_16_024935) do
+ActiveRecord::Schema.define(version: 2024_10_16_025407) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "level", default: 1, null: false
+    t.integer "category_id", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_tests_on_author_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +36,6 @@ ActiveRecord::Schema.define(version: 2024_10_16_024935) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
