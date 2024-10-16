405542b8b8cb8a7491309e46e80a4a7d6afcdc5a
a.y.kuznetsov <a.y.kuznetsov@fun-box.ru>
2024-10-16 06:55:11 +0300

    feat: добавление модели ответа

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_16_034120) do
+ActiveRecord::Schema.define(version: 2024_10_16_034456) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.boolean "correct", default: false, null: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
@@ -44,6 +53,7 @@ ActiveRecord::Schema.define(version: 2024_10_16_034120) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
